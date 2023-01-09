6d2ac1c741f3a1e2b3fc3ef6ff0674187daf8419
Filippo Sbaiz <sbaizfilippo@gmail.com>
2023-01-09 15:51:40 +0100

    debug

--- js/script.js
+++ js/script.js
@@ -3,23 +3,20 @@ const { createApp } = Vue
 createApp({
 data() {
     return {
-        minimo: 1,
-        massimo: 10,
         arrayEmail: [],
     }
 },
 methods: {
     generateArray (){
-        axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`, { params: { min: this.minimo, max: this.massimo}}).then((result) => {
-            // console.log(result.data.response);
-        });
 
         for (let i=0; i<10; i++){
             axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((result) => {
             this.arrayEmail.push(result.data.response)
             });
         }
+
         console.log(this.arrayEmail)
+
     }
 }
 }).mount('#app')
