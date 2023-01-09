5f1aadff6e2c7573cda54325213e5ac1020a1e5a
Filippo Sbaiz <sbaizfilippo@gmail.com>
2023-01-09 16:54:46 +0100

    debug

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ data() {
 },
 methods: {
     generateArray (){
-
+        this.arrayEmail = []
         for (let i=0; i<10; i++){
             axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((result) => {
             this.arrayEmail.push(result.data.response)
@@ -16,7 +16,7 @@ methods: {
         }
 
         console.log(this.arrayEmail)
-        
+
     }
 }
 }).mount('#app')
